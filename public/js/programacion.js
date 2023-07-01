ae8cd1174f5a4104bdca95fab3ca3dc63bfc90b4
Ezequiel Arroyo <ezarroyo@frba.utn.edu.ar>
2023-07-01 19:37:31 -0300

    funcion typeWriter

--- public/js/programacion.js
+++ public/js/programacion.js
@@ -1,5 +1,10 @@
 const redes = Array.from (document.getElementsByClassName("red_icono"));
-//Funcion que activa la clase "bounce" en los iconos de mis redes sociales
+
+var i = 0; /* contador */
+var txtSobreMi = 'Hola,soy Ezequiel Arroyo.Tengo 24 años y vivo en Argentina.Me gusta la programacion y la jardineria. Soy una persona creativa, paciente y persistente, con buenas relaciones interpersonales.Siempre tengo la disposición para ayudar a las personas y aprender de mis errores.'; /* The text */
+var speed = 30; /* The speed/duration of the effect in milliseconds */ 
+
+
 redes.forEach(red => {
     red.addEventListener("mouseover", () => {
         red.classList.add("fa-bounce")
@@ -8,3 +13,13 @@ redes.forEach(red => {
         red.classList.remove('fa-bounce')
     });
 });
+
+document.addEventListener('DOMContentLoaded', typeWriter)
+
+function typeWriter() {   
+    if (i < txtSobreMi.length) {
+        document.getElementById("sobreMi").innerHTML += txtSobreMi.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
